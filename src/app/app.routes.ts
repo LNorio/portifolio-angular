66560fa6bb6a13d820864f73cb2e8e8fcd7ab042
Leonardo Norio <leonardocruz@alunos.utfpr.edu.br>
2024-02-19 20:02:43 -0300

    primeiro commit portifolio - home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HistoriaComponent } from '../components/historia/historia.component';
+import { HomeComponent } from '../components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'historia', component: HistoriaComponent},
+    {path: '', component: HomeComponent},
+    {path: '**', component: HomeComponent}
+];
